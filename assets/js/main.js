ad1c5f23057899cc8e744a62a4f3b863a1a268f9
W-Laboratories <ikebusinessoff@gmail.com>
2022-09-11 22:10:37 +0800

    organised sections and hover properties

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -49,7 +43,7 @@
 				speed: 1500,
 				offset: $header.outerHeight()
 			});
-
+	
 	// Menu.
 		$('#menu')
 			.append('<a href="#menu" class="close"></a>')
