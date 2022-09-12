b145ce053a8b4ab8b235a0c0deaafa765795562d
W-Laboratories <ikebusinessoff@gmail.com>
2022-09-13 02:24:17 +0800

    finished home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 (function($) {
-
+	function randomQuote() {
+		$.ajax({
+			url: "https://api.forismatic.com/api/1.0/?",
+			dataType: "jsonp",
+			data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+			success: function( response ) {
+			  $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
+				response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+			  $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
+				' (' + response.quoteAuthor + ')');
+			}
+		});
+	  }
+	randomQuote();
+	
 	var	$window = $(window),
 		$body = $('body'),
 		$wrapper = $('#page-wrapper'),
